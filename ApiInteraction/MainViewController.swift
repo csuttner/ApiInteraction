9cac40ed39f2d37f57e0c7dc5428bba5c03a7fff
Clay Suttner <csuttner@gmail.com>
2021-06-11 12:30:48 -0700

    Adds ViewController and Nib for login success screen

---
+++ ApiInteraction/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  ApiInteraction
+//
+//  Created by Clay Suttner on 6/11/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ApiInteraction/SuccessViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessViewController.swift
+//  ApiInteraction
+//
+//  Created by Clay Suttner on 6/11/21.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
