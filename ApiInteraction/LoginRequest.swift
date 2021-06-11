e580e4c85312503ea7e496bf34e66d591235a91f
Clay Suttner <csuttner@gmail.com>
2021-06-11 15:31:45 -0700

    Refactors login model to be more descriptive and implements basic post request with debug print statement

--- ApiInteraction/LoginRequest.swift
+++ ApiInteraction/LoginRequest.swift
@@ -8,13 +8,19 @@
 import Foundation
 import Alamofire
 
-struct LoginResponseModel: Codable {
+struct LoginModel: Codable {
     let email: String
     let password: String
 }
 
 struct LoginRequest {
     static func post() {
-
+        let baseURLString = "https://games-for-good-demo.herokuapp.com/login"
+        let headers: HTTPHeaders = [
+            .contentType("application/json")
+        ]
+        AF.request(baseURLString, method: .post, parameters: LoginModel(email: "rkoch@mindgrub.com", password: "password"), encoder: JSONParameterEncoder.default, headers: headers).response { response in
+            debugPrint(response)
+        }
     }
 }
