827a30e4338fadce1af459a07e7d555c2f64cdb9
Clay Suttner <csuttner@gmail.com>
2021-06-11 11:54:57 -0700

    Initial commit with base project

---
+++ ApiInteraction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiInteraction
+//
+//  Created by Clay Suttner on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
