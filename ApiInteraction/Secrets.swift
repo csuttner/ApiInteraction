a6d3a5e7049cd5842f248772cccc21c499adbb6e
Clay Suttner <csuttner@gmail.com>
2021-06-11 17:39:54 -0700

    Changes number of lines to 0 on token label to show differences each network call

---
+++ ApiInteraction/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  ApiInteraction
+//
+//  Created by Clay Suttner on 6/11/21.
+//
+
+import Foundation
+
+struct Secrets {
+    static let baseURLString = "https://games-for-good-demo.herokuapp.com/login"
+}
